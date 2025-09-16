29953fd0e941e39e49d2ba5c77f3e1e3491c7aab
Cristh <criisth55@outlook.com>
2025-09-16 14:24:06 -0600

    Se eliminaron filas redundantes como para crear diccionario. Se eliminaron líneas comentadas y comentarios irrelevantes. Se añadieron funciones para crear gráfica. Se agruparon las medidas estadísticas

--- Department_report.py
+++ Department_report.py
@@ -24,9 +24,10 @@ import matplotlib.pyplot as plt
 from functools import partial
 Timer0 = time.time()
 
-class TablaDeDatos: ##Crea una tabla en matplotlib.
+
+class TablaDeDatos: ##Crea una tabla en matplotlib y le da formato de moneda
     def __init__(self, df): ##Init es un objeto que se inicializa al crear una clase.
-        # Solo guardamos el DataFrame, no creamos nada aún
+        #Inicializamos objetos
         self.df = df ##Guardamos el dataframe.
         self.fig = None ##preparamos fig
         self.ax = None ##lugar para dibujar
@@ -34,26 +35,28 @@ class TablaDeDatos: ##Crea una tabla en matplotlib.
 
     def crear(self):
         fig, ax = plt.subplots(figsize=(12, 4))#ax == un eje es el lienzo donde se dibuja.
-        ax.axis("off")  # controla la visualización de bordes, ticks, etc.
+        ax.axis("off")  # contro1la la visualización de bordes, ticks, etc.
         # Crear tabla con los datos de tu DataFrame
         tabla = ax.table( ##ax.table dibuja una tabla sobre el eje y devuelve un objeto Table
-            cellText=df_report.values,       #valores de la tabla, nuestro dataframe en este caso.
-            colLabels=df_report.columns,     #nombres de columnas
+            cellText=self.df.values,       #valores de la tabla, nuestro dataframe en este caso.
+            colLabels=self.df.columns,     #nombres de columnas
             loc="center")                     #posición centrada
         # Ajustar formato
         tabla.auto_set_font_size(False)
         tabla.set_fontsize(10)
         tabla.scale(1.2, 1.2) #escala (ancho, alto)
+        for col in self.df.iloc[:, 1:].columns: ##Da formato de moneda, pero convierte en string.
+            self.df[col] = self.df[col].map("${:,.2f}".format)
+            
 
-
-#Callback: cuando hagas click, buscamos si el click “cayó” dentro de alguna barra
-def on_click(event):
+def on_click(event): #Acciona evento (tilt) al hacer click.
+def on_click(event, ax, bars, fig):
     if event.inaxes is not ax:  # ignora clics fuera del eje
         return
     for rect in bars:
         contains, _ = rect.contains(event)  # ¿el click está dentro del rectángulo?
         if contains:
-            print(f"Click en barra: {rect._dept}")
+            # print(f"Click en barra: {rect._dept}")
             rect.set_linewidth(1.5)
             rect.set_edgecolor("black")
             fig.canvas.draw_idle() ##Esto pide el redibujo
@@ -64,59 +67,56 @@ def on_click(event):
                 fig.canvas.draw_idle()
             timer.add_callback(restore_edge)
             timer.start()
-
-
             break
+
+def crear_graph_bars(df): ##Esta función crea gráficas de barra
+    ax = df.plot(kind="bar", x="Department", y="Monto_por_departamento", legend=False) ##Crea las gráficas
+    fig = ax.figure
+    bars = ax.containers[0] #todas las barras
+    for rect, dept in zip(bars, df["Department"]):
+        rect._dept = dept  # le cuelgas el nombre del depto a cada barra
+    cid = fig.canvas.mpl_connect("button_press_event", 
+        lambda event:on_click(event, ax, bars,fig))
+    
+    return fig, ax, bars, cid
 
 
 if __name__ == "__main__":
     ruta_completa = r"C:\Users\criis\Documents\Coding\Ejemplo Transacciones por report.csv"
     df = pd.read_csv(ruta_completa, encoding="latin-1", header=0)
-    #2.- Almacenar en un diccionario los departamentos.
-    dict_depto = {}
-    for valor in df.iloc[:,1]:
-        if valor not in dict_depto:
-            dict_depto[valor] = None
-    #3.- Crea un diccionario por departamento con los montos.
     serie_depto = df.groupby("Department")[df.columns[4]].sum()#.to_dict() #Agrupa y crea una serie con las sumas.
-    # dict_depto = serie_depto.to_dict() ##Convierte la serie a diccionario.
-
-    #Sacamos medidas creando series.
+    #Medidas creando series.
     DE = df.groupby("Department")[df.columns[4]].std()
     promedio = df.groupby("Department")[df.columns[4]].mean()
-
 
     #Convertimos series a dataframe
     DE_idx = DE.to_frame(name="Standard deviation").reset_index()
     promedio_idx = promedio.to_frame(name="Average").reset_index()
-    CV = ((DE_idx["Standard deviation"] / promedio_idx["Average"]) * 100).to_frame(name="CV").reset_index()
+    CV = ((DE_idx["Standard deviation"] / promedio_idx["Average"]) * 100) #Coeficiente de variacion = (DE/avg)*100
+    CV_idx = CV.to_frame(name="CV").reset_index()
 
-    #Coeficiente de variacion = (DE/avg)*100
-    # CV = ((DE_idx["Standard_deviation"] / promedio_idx["Average"]) * 100).to_frame(name="CV").reset_index()
-
+    #Armamos el dataframe
     df_report = df.groupby("Department")[df.columns[4]].agg(Monto_por_departamento="sum").reset_index()
     df_report = df_report.merge(DE_idx, on="Department", how="left")
     df_report = df_report.merge(promedio_idx, on="Department", how="left")
-
-    ax = df_report.plot(kind="bar", x="Department", y="Monto_por_departamento", legend=False) ##Crea las gráficas
-    fig = ax.figure
-    bars = ax.containers[0] #todas las barras
-    for rect, dept in zip(bars, df_report["Department"]):
-        rect._dept = dept  # le cuelgas el nombre del depto a cada barra
-    
-    for col in df_report.iloc[:,[1,2,3]].columns: ##Le damos formato de número al dataframe.
-        df_report[col] = df_report[col].map("${:,.2f}".format)
-    
-    cid = fig.canvas.mpl_connect("button_press_event", on_click)
     # df_report = df_report.merge(CV, on="Department", how="left")
     # df_report = df.groupby("Department")[df.columns[4]].agg(amount_sum = "sum", amount_avg= "mean").reset_index()
+    # tabla = TablaDeDatos(df_report)
+    # tabla.crear()
+    crear_graph_bars(df_report)
+
+    
+    
+    
+    
+    
+    
     
 
 
     Timer1 = time.time()
     ExecTime = Timer1-Timer0
     print(f"Tiempo de ejecución == {ExecTime}")
-
     plt.show()
     # print(df_report)
     # print(df_report.to_string())
